f49a047a35fb1c7c6f075dd17861ca22f003be73
johnnataa <johnnata_@outlook.com>
2024-01-17 16:49:23 -0300

    first commit

---
+++ src/main/java/com/generation/gamestore/model/Categorias.java
@@ -0,0 +1,75 @@
+package com.generation.gamestore.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(max = 50)
+	@NotBlank(message = "O Gênero é obrigatório!")
+	private String genero;
+	
+	@Size(max = 255)
+	@NotNull(message = "A Descrição é obrigatória")
+	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categorias", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categorias")
+	private List<Produtos> produtos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produtos> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/generation/gamestore/model/Produtos.java
@@ -0,0 +1,114 @@
+package com.generation.gamestore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Size(max = 50)
+	@NotBlank(message = "O nome é obrigatório!")
+	private String nome;
+
+	@Size(min = 10, max = 10)
+	@NotBlank(message = "A Data de lançamento é obrigatória!")
+	private String dataDeLancamento;
+
+	private Integer classificacao;
+
+	@Size(max = 50)
+	private String desenvolvedora;
+
+	@Size(max = 50)
+	private String distribuidora;
+
+	private Double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categorias categorias;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataDeLancamento() {
+		return dataDeLancamento;
+	}
+
+	public void setDataDeLancamento(String dataDeLancamento) {
+		this.dataDeLancamento = dataDeLancamento;
+	}
+
+	public int getClassificacao() {
+		return classificacao;
+	}
+
+	public void setClassificacao(int classificacao) {
+		this.classificacao = classificacao;
+	}
+
+	public String getDesenvolvedora() {
+		return desenvolvedora;
+	}
+
+	public void setDesenvolvedora(String desenvolvedora) {
+		this.desenvolvedora = desenvolvedora;
+	}
+
+	public String getDistribuidora() {
+		return distribuidora;
+	}
+
+	public void setDistribuidora(String distribuidora) {
+		this.distribuidora = distribuidora;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categorias getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(Categorias categorias) {
+		this.categorias = categorias;
+	}
+	
+	
+
+	
+	
+
+}
